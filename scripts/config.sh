422ff30268ee8c40dfff5671fd5b1955267aa2be
ELEPOT <elepotmail0@gmail.com>
2025-09-13 23:51:05 +0800

    a

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,8 +6,10 @@ nvim -p home.nix nixos/configuration.nix flake.nix
 alejandra . &>/dev/null
 git diff -U0 *.nix
 echo "NixOS Rebuilding..."
+git update-index --no-assume-unchanged device_specific/* nixos/hardware_configuration.nix
 sudo nixos-rebuild switch --flake . --impure
 current=$(nixos-rebuild list-generations | grep True | awk '{print $1}')
+git update-index --assume-unchanged device_specific/* nixos/hardware_configuration.nix
 git commit -am "$current"
 popd
 notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
